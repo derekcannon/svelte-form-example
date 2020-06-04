050fa0727c02ffe1c56084a4ef638302a363ffec
Derek Cannon <derektcannon@gmail.com>
2020-06-04 12:01:24 -0700

    Run Prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,18 @@
 <script>
-	import Tailwindcss from "./Tailwindcss.svelte";
-	import Form from "./Form.svelte";
+  import Tailwindcss from "./Tailwindcss.svelte";
+  import Form from "./Form.svelte";
 </script>
 
 <style>
-	:global(body) {
-		@apply p-10 bg-gray-300;
-	}
+  :global(body) {
+    @apply p-10 bg-gray-300;
+  }
 </style>
 
 <Tailwindcss />
 
 <main>
-	<div class="border border-gray-400 p-4 bg-white">
-		<Form />
-	</div>
+  <div class="border border-gray-400 p-4 bg-white">
+    <Form />
+  </div>
 </main>
-
--- src/Input.svelte
+++ src/Input.svelte
@@ -1,25 +1,25 @@
 <script>
-    export let label;
-    export let value;
-    export let blur;
-    export let error;
+  export let label;
+  export let value;
+  export let blur;
+  export let error;
 </script>
 
 <div class="pb-4">
-    <label class="flex">
-        {#if label}
-            <span class="whitespace-no-wrap pr-2">{label}:</span>
-        {/if}
+  <label class="flex">
+    {#if label}
+      <span class="whitespace-no-wrap pr-2">{label}:</span>
+    {/if}
 
-        <div class="w-full">
-            <input
-                class="w-full box-border border border-gray-400 focus:border-blue-500 pl-1 pr-1"
-                bind:value
-                on:blur
-            >
-            {#if error}
-                <div class="text-red-500 italic text-sm">{error}</div>
-            {/if}
-        </div>
-    </label>
+    <div class="w-full">
+      <input
+        class="w-full box-border border border-gray-400 focus:border-blue-500
+        pl-1 pr-1"
+        bind:value
+        on:blur />
+      {#if error}
+        <div class="text-red-500 italic text-sm">{error}</div>
+      {/if}
+    </div>
+  </label>
 </div>
