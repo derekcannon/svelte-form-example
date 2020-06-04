7888f4582a866079780296d808e712c716765492
Derek Cannon <derektcannon@gmail.com>
2020-06-04 00:42:01 -0700

    Add Tailwind CSS

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,16 @@
 <script>
-	export let name;
+	import Tailwindcss from "./Tailwindcss.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.description {
+		@apply text-xl text-gray-700;
 	}
 </style>
+
+<main>
+	<div class="text-6xl text-orange-500">Hello there!</div>
+	<div class="description">Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</div>
+</main>
+
+<Tailwindcss />
